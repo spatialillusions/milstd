3298be24ae5baefeea3b34c6724662b40e506e5f
Måns Beckman <mans.beckman@spatialillusions.com>
2018-04-28 09:53:45 +0200

    Update with readme and stuff

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,12 @@ import json from "rollup-plugin-json";
 export default [
   {
     // Generate JSON for all standards
-    input: "JSON/milstd.js",
+    input: "milstd.js",
     output: [
-      {
+      /*{
         file: "milstd.js",
         format: "es"
-      },
+      },*/
       {
         file: "milstd-umd.js",
         format: "umd",
