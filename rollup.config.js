546f260d00c2b86b0b411d6cf8a830de19269d25
Måns Beckman <mans.beckman@spatialillusions.com>
2018-04-28 10:28:31 +0200

    Fixed build to ES module

--- rollup.config.js
+++ rollup.config.js
@@ -3,17 +3,17 @@ import json from "rollup-plugin-json";
 export default [
   {
     // Generate JSON for all standards
-    input: "milstd.js",
+    input: "JSON/milstd.js",
     output: [
-      /*{
+      {
         file: "milstd.js",
         format: "es"
-      },*/
+      } /*,
       {
         file: "milstd-umd.js",
         format: "umd",
         name: "milstd"
-      }
+      }*/
     ],
     plugins: [json()]
   }
